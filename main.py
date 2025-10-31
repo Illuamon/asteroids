55743f88a9652eeea22ef7e027e244a803fc29b1
Illuamon <nsleskova@protonmail.com>
2025-10-31 14:17:11 +0100

    B: draws player triangle on screen

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
+import player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = 60
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    the_clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    triangle = player.Player(x, y, PLAYER_RADIUS)
 
     #game loop
     while True:
@@ -15,6 +26,10 @@ def main():
                 return
             
         screen.fill(color='black')
+        dt = the_clock.tick(fps) / 1000
+
+        triangle.draw(screen)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
